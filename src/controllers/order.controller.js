a58960ac594257684b2454ee3547e65886d18a26
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-02 23:23:26 +0700

    update

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -48,6 +48,17 @@ function OrderController() {
     }
   };
 
+  this.getAllOrdersForCurrentUser = async (req, res) => {
+    try {
+      const userId = req.user._id;
+      const orders = await Order.find({ user: userId });
+      res.status(200).json({ orders });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error" });
+    }
+  };
+
   return this;
 }
 
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -75,6 +75,19 @@ function ProductController() {
     }
   };
 
+  this.getRemainingQuantity = async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Not Found!" });
+      }
+      const remainingQuantity = product.quantity;
+      res.status(200).json({ remainingQuantity: remainingQuantity });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+
   return this;
 }
 
