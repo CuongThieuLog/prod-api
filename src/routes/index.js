00ee62ffcb1be3733bb513d97e0bd281bffc686c
CuongThieuLog <thieutrancuong.log@gmail.com>
2024-04-02 15:22:04 +0700

    Setup base

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const userRoute = require("./user.routes");
+const authRoute = require("./auth.routes");
+const exampleRoute = require("./example.routes");
+
+const router = express.Router();
+
+router.use("/", userRoute);
+router.use("/", authRoute);
+router.use("/example/", exampleRoute);
+
+module.exports = router;
